9254c55a4e58708b07a29f2b574343a93b5b2d60
Kyle Leichtle <kyle.leichtle@target.com>
2016-06-08 10:30:03 -0500

    first

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        // "quotes": [
+        //     "error",
+        //     "double"
+        // ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "warn", 
+            { 
+                "vars": "all",
+                "args": "after-used" 
+            }
+        ],
+        "no-console": [0]
+    }
+};
---
+++ data/schema.js
@@ -0,0 +1,31 @@
+const typeDefinitions = `
+type Query {
+  testString: String
+}
+
+type Author{
+    id: Int
+    firstName: String
+    lastName: String
+    posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+
+type Query {
+    author(firstName: String, lastName: String): Author
+    getFortuneCookie: String # we'll use this later
+}
+
+schema {
+  query: Query
+}
+`;
+
+export default [typeDefinitions];
